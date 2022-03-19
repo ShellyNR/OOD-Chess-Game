eafe5233c0080d66d0fc9bf12f685bddc3336a4a
barshein <82324960+barshein@users.noreply.github.com>
2022-03-19 17:55:08 +0200

    Update common.java

--- src/Tools/common.java
+++ src/Tools/common.java
@@ -18,7 +18,7 @@ public class common {
         toolsCache.put(EnumTool.BishopW, new Bishop(true));
         toolsCache.put(EnumTool.BishopB, new Bishop(false));
         toolsCache.put(EnumTool.KnightW, new Knight(true)); // is n in char[][]
-        toolsCache.put(EnumTool.KnightB, new Knight(false));
+        toolsCache.put(EnumTool.KnightB, new Knight(false)); // is n in char[][]
         toolsCache.put(EnumTool.PawnW, new Pawn(true));
         toolsCache.put(EnumTool.PawnB, new Pawn(false));
         toolsCache.put(EnumTool.Empty, new EmptyTool(false));
