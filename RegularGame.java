595c01843db725a851f1c3ffae0b7d6f186b9bd8
barshein <82324960+barshein@users.noreply.github.com>
2022-03-19 18:01:27 +0200

    Add files via upload

---
+++ RegularGame.java
@@ -0,0 +1,47 @@
+package Game;
+
+import Component.Board;
+import Player.*;
+import GameHandler.*;
+import BoardUI.UIBoard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegularGame implements Game{
+
+    private GameHandler gameHendler;
+    private List<Player> playersList;
+    private Board initBoard;
+    private UIBoard UIb;
+
+
+    public RegularGame() {
+        this.initBoard = new Board(); // create defualt board
+        this.playersList = createPlayersList();
+        this.gameHendler = new RegularGameHandler(this.playersList,this.initBoard);
+
+    }
+
+    @Override
+    public void setUIBoard(UIBoard UIB) {
+        this.UIb = UIB;
+    }
+
+    public List<Player> createPlayersList() {
+        List<Player> playersList = new ArrayList<>();
+        playersList.add(new HumanPlayer(true));
+        playersList.add(new HumanPlayer(false));
+        return playersList;
+    }
+
+    public String toString(){
+        return "Regular game";
+    }
+
+    public void runGame() {
+        this.gameHendler.handleGame();
+    }
+
+
+}
---
+++ WatchGame.java
@@ -0,0 +1,70 @@
+package Game;
+
+import BoardUI.UIBoard;
+import Component.Board;
+import FamousPlay.*;
+import GameHandler.*;
+import Player.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WatchGame implements Game {
+
+
+    private GameHandler gameHendler;
+    private List<Player> playersList;
+    private List<FamousPlay> watchList;
+    private FamousPlay chosenPlay;
+    private Board initBoard;
+    private UIBoard UIb;
+
+
+    public WatchGame() {
+        this.watchList = setWatchList();
+        this.chosenPlay = generatePlay();
+        this.initBoard = new Board(this.chosenPlay.getInitState()); // create default board
+        this.playersList = createPlayersList();
+        this.gameHendler = new WatchGameHandler(this.playersList,this.initBoard,this.chosenPlay.isAPlayerStart());
+
+    }
+
+    public List<FamousPlay> setWatchList() {
+        List<FamousPlay> listP = new ArrayList<>();
+        listP.add(new PlayA());
+        return listP;
+
+    }
+
+    public FamousPlay generatePlay(){
+        Random ran = new Random();
+
+        // Returns number between 0 to (list_size-1)
+        int index = ran.nextInt(this.watchList.size() - 1);
+        return this.watchList.get(index);
+
+    }
+
+    @Override
+    public void setUIBoard(UIBoard UIB) {
+
+    }
+
+    public List<Player> createPlayersList() {
+        List<Player> playersList = new ArrayList<>();
+        playersList.add(new ComputerPlayer(true,this.chosenPlay.getAPlayerMovesForSolveQuiz()));
+        playersList.add(new ComputerPlayer(false, this.chosenPlay.getBPlayerMovesForSolveQuiz()));
+        return playersList;
+    }
+
+    public String toString(){
+        return "Watch game";
+    }
+
+    public void runGame() {
+        this.gameHendler.handleGame();
+    }
+
+
+}
