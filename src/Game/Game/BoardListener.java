bf7f4e086c456dccb440e27b53c06cc17523deae
ShellyNR <73890534+ShellyNR@users.noreply.github.com>
2022-03-23 03:43:28 +0200

    Delete src/Game/Game directory

--- src/Game/Game/BoardListener.java
+++
@@ -1,9 +0,0 @@
-package Game;
-import Component.Move;
-
-public interface BoardListener {
-    // notify about a new move from the ui
-    public void notify(Move move);
-    // notify that the user asks to start a new game
-    public void notifyNew();
-}
