e27e0c8e734074eeb5bf846c3606485c5b102176
ShellyNR <73890534+ShellyNR@users.noreply.github.com>
2022-03-23 03:40:53 +0200

    Command pattern

---
+++ src/ListenerToBoard/BoardNotifier.java
@@ -0,0 +1,20 @@
+package ListenerToBoard;
+
+import Game.*;
+import Component.*;
+import java.awt.*;
+
+public interface BoardNotifier {
+
+    // add a component the listens to the notifier
+    public void addBoardListener(BoardListener ml);
+
+    // add a component that the notifier needs to listen to
+    public Object addBoardNotifier(Object c);
+
+    // notify listeners when the user asks for a new game
+    public void NotifyNewBoard();
+
+    // notify the listeners whan the user makes a move on the board
+    public void NotifyNewMove(Move move);
+}
---
+++ src/ListenerToBoard/MouseListenerToBoard.java
@@ -0,0 +1,107 @@
+package ListenerToBoard;
+
+import Game.BoardListener;
+import Component.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MouseListenerToBoard implements MouseListener, BoardNotifier {
+    private int count = 0;
+    private JButton chessPiece;
+    private long lastclick;
+    private List<BoardListener> boardListener =  new ArrayList<>();
+    Move currentMove;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // if the two clicks are too close
+        if(System.currentTimeMillis() < lastclick + 20) return;
+        lastclick = System.currentTimeMillis();
+
+        // the first click - source
+        if (this.count == 0) {
+            chessPiece = null;
+            Component c = e.getComponent();
+            if (!(c instanceof JButton)) return;
+            chessPiece = (JButton) c;
+            int w=chessPiece.getSize().width;
+            int h=chessPiece.getSize().height;
+            int x = chessPiece.getX() / w - 1;
+            int y = chessPiece.getY() / h - 1;
+            // initiate new move but without the start tool
+            this.currentMove = new Move(new Spot(y, x, null));
+            this.count = 1;
+        } else if (this.count == 1) {
+            // the second click
+            if (chessPiece == null) return;
+            Component c = e.getComponent();
+            chessPiece = (JButton) c;
+            int w=chessPiece.getSize().width;
+            int h=chessPiece.getSize().height;
+            int x = chessPiece.getX() / w - 1;
+            int y = chessPiece.getY() / h - 1;
+
+            // the move.end does not have a tool yet
+            this.currentMove.setEnd(new Spot(y, x, null));
+
+            // notify listeners about the move
+            for (BoardListener ml:this.boardListener) {
+                ml.notify(this.currentMove);
+            }
+            chessPiece = null;
+            this.count = 0;
+        }
+
+    }
+
+    //@Override
+    public void addBoardListener(BoardListener ml) {
+        this.boardListener.add(ml);
+    }
+
+    @Override
+    public Object addBoardNotifier(Object o) {
+        Component c = (Component)o;
+        c.addMouseListener(this);
+        return c;
+    }
+
+    @Override
+    public void NotifyNewBoard() {
+        for (BoardListener ml:this.boardListener) {
+            ml.notifyNew();
+        }
+    }
+
+    @Override
+    public void NotifyNewMove(Move move) {
+        for (BoardListener ml:this.boardListener) {
+            ml.notify(move);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
